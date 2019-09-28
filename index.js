ab981f24dd87b03c6319eef243b5081a229777ef
jorcalle11 <jorcalle11@gmail.com>
2019-09-28 15:27:55 -0500

    Add redis to save the number of visits

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 'use strict';
 
 const express = require('express');
+const redis = require('redis');
 const os = require('os');
 
 const app = express();
+const redisClient = redis.createClient();
 const port = process.env.PORT || 3000;
+const REDIS_KEY = 'visits';
 
 app.use('/', (req, res) => {
   const hostname = os.hostname();
   const homedir = os.homedir();
-  res.send({ hostname, homedir });
+
+  redisClient.get(REDIS_KEY, (err, visits) => {
+    const numberOfVisits = visits ? +visits : 0;
+    redisClient.set(REDIS_KEY, numberOfVisits + 1);
+
+    res.send({ hostname, homedir, visits: numberOfVisits });
+  });
 });
 
 app.listen(port, () => {
